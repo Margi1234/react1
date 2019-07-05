f537c03728002e749e659df52c5143526dffe3dd
Margi1234 <margi.panjvani123@gmail.com>
2019-07-05 16:01:11 +0530

    commited

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+// const element = <h1>Hello World!</h1>;
+// console.log(element);
+
+import Todo from "./components/to-do-list";
+ReactDOM.render(<Todo />, document.getElementById("root"));
